d1665816daa1b21ab3b5d3e9bd616e62ba3144e1
Max Bernstein <max@bernsteinbear.com>
2023-11-15 22:47:41 -0500

    Add string, concat, type checking tests

--- scrapscript.py
+++ scrapscript.py
@@ -15,12 +15,18 @@ class Int(Object):
 
 
 @dataclass(eq=True, frozen=True, unsafe_hash=True, repr=False)
+class String(Object):
+    value: str
+
+
+@dataclass(eq=True, frozen=True, unsafe_hash=True, repr=False)
 class Var(Object):
     name: str
 
 
 class BinopKind(enum.Enum):
     ADD = auto()
+    CONCAT = auto()
     SUB = auto()
     MUL = auto()
     DIV = auto()
@@ -36,12 +42,20 @@ class Binop(Object):
 def eval_int(env: dict[str, Object], exp: Object) -> int:
     result = eval(env, exp)
     if not isinstance(result, Int):
-        raise TypeError(f"expected Int, got {result}")
+        raise TypeError(f"expected Int, got {type(result).__name__}")
+    return result.value
+
+
+def eval_str(env: dict[str, Object], exp: Object) -> str:
+    result = eval(env, exp)
+    if not isinstance(result, String):
+        raise TypeError(f"expected String, got {type(result).__name__}")
     return result.value
 
 
 BINOP_HANDLERS = {
     BinopKind.ADD: lambda env, x, y: Int(eval_int(env, x) + eval_int(env, y)),
+    BinopKind.CONCAT: lambda env, x, y: String(eval_str(env, x) + eval_str(env, y)),
     BinopKind.SUB: lambda env, x, y: Int(eval_int(env, x) - eval_int(env, y)),
     BinopKind.MUL: lambda env, x, y: Int(eval_int(env, x) * eval_int(env, y)),
     BinopKind.DIV: lambda env, x, y: Int(eval_int(env, x) // eval_int(env, y)),
@@ -49,7 +63,7 @@ BINOP_HANDLERS = {
 
 
 def eval(env: dict[str, Object], exp: Object) -> Object:
-    if isinstance(exp, Int):
+    if isinstance(exp, (Int, String)):
         return exp
     if isinstance(exp, Var):
         value = env.get(exp.name)
@@ -69,6 +83,10 @@ class EvalTests(unittest.TestCase):
         exp = Int(5)
         self.assertEqual(eval({}, exp), Int(5))
 
+    def test_eval_str_returns_str(self) -> None:
+        exp = String("xyz")
+        self.assertEqual(eval({}, exp), String("xyz"))
+
     def test_eval_with_non_existent_var_raises_name_error(self) -> None:
         exp = Var("no")
         with self.assertRaises(NameError) as ctx:
@@ -84,6 +102,12 @@ class EvalTests(unittest.TestCase):
         exp = Binop(BinopKind.ADD, Int(1), Int(2))
         self.assertEqual(eval({}, exp), Int(3))
 
+    def test_eval_with_binop_add_with_int_string_raises_type_error(self) -> None:
+        exp = Binop(BinopKind.ADD, Int(1), String("hello"))
+        with self.assertRaises(TypeError) as ctx:
+            eval({}, exp)
+        self.assertEqual(ctx.exception.args[0], "expected Int, got String")
+
     def test_eval_with_binop_sub(self) -> None:
         exp = Binop(BinopKind.SUB, Int(1), Int(2))
         self.assertEqual(eval({}, exp), Int(-1))
@@ -95,6 +119,22 @@ class EvalTests(unittest.TestCase):
     def test_eval_with_binop_div(self) -> None:
         exp = Binop(BinopKind.DIV, Int(2), Int(3))
         self.assertEqual(eval({}, exp), Int(0))
+
+    def test_eval_with_binop_concat_with_strings_returns_string(self) -> None:
+        exp = Binop(BinopKind.CONCAT, String("hello"), String(" world"))
+        self.assertEqual(eval({}, exp), String("hello world"))
+
+    def test_eval_with_binop_concat_with_int_string_raises_type_error(self) -> None:
+        exp = Binop(BinopKind.CONCAT, Int(123), String(" world"))
+        with self.assertRaises(TypeError) as ctx:
+            eval({}, exp)
+        self.assertEqual(ctx.exception.args[0], "expected String, got Int")
+
+    def test_eval_with_binop_concat_with_string_int_raises_type_error(self) -> None:
+        exp = Binop(BinopKind.CONCAT, String(" world"), Int(123))
+        with self.assertRaises(TypeError) as ctx:
+            eval({}, exp)
+        self.assertEqual(ctx.exception.args[0], "expected String, got Int")
 
 
 if __name__ == "__main__":

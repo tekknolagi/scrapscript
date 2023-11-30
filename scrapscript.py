de57a72a88a334729633f6514a1564b4234dd1d4
Max Bernstein <max@bernsteinbear.com>
2023-11-30 10:56:09 -0500

    Link to repl page

--- scrapscript.py
+++ scrapscript.py
@@ -1237,7 +1237,7 @@ button.addEventListener("click", () => {
         self.send_response(404)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(b"try hitting /eval?exp=EXP&env=ENV")
+        self.wfile.write(b"""try hitting <a href="/repl">/repl</a>""")
         return
 
     def do_eval(self, query: Dict[str, Any]) -> None:

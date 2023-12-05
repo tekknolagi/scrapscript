dd6b7941c0ce10f3cfc0029aa02e311284d3d709
Max Bernstein <max@bernsteinbear.com>
2023-12-05 13:37:56 -0500

    Allow reuse of address
    This means we can C-c the server and re-start it on the same port
    immediately.

--- scrapscript.py
+++ scrapscript.py
@@ -3376,6 +3376,7 @@ def serve_command(args: argparse.Namespace) -> None:
         server = socketserver.ForkingTCPServer
     else:
         server = socketserver.TCPServer
+    server.allow_reuse_address = True
     with server(("", args.port), ScrapReplServer) as httpd:
         print("serving at port", args.port)
         httpd.serve_forever()

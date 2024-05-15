647a9da1af8c3edfeb79604afb7c861e8973ba1b
Max Bernstein <max@bernsteinbear.com>
2024-05-15 16:18:37 -0400

    Support user-defined functions without closure variables

--- compiler.py
+++ compiler.py
@@ -1,22 +1,61 @@
-from dataclasses import dataclass
-from scrapscript import Object, Binop, Int, String, BinopKind, parse, tokenize, Where, Assign, Var, Apply, List
+import dataclasses
+import itertools
+from scrapscript import (
+    Apply,
+    Assign,
+    Binop,
+    BinopKind,
+    Function,
+    Int,
+    List,
+    Object,
+    String,
+    Var,
+    Where,
+    free_in,
+    parse,
+    tokenize,
+)
 
 Env = dict[str, str]
 
 
+fn_counter = itertools.count()
+
+
+@dataclasses.dataclass
+class Closure:
+    id: int = dataclasses.field(default=0, init=False, compare=False, hash=False)
+    name: str
+    params: list[str]
+    fields: list[str] = dataclasses.field(default_factory=list)
+    code: list[str] = dataclasses.field(default_factory=list)
+
+    def __post_init__(self) -> None:
+        self.id = next(fn_counter)
+        self.code.append("HANDLES();")
+
+    def decl(self) -> str:
+        args = ", ".join(f"struct gc_obj* {arg}" for arg in self.params)
+        return f"struct gc_obj* {self.name}({args})"
+
+
 class Compiler:
-    def __init__(self) -> None:
-        self.code: list[str] = []
+    def __init__(self, main: Closure) -> None:
         self.gensym_counter: int = 0
+        self.functions: list[Closure] = [main]
+        self.function: Closure = main
 
     def gensym(self) -> str:
         self.gensym_counter += 1
         return f"tmp_{self.gensym_counter-1}"
 
+    def _emit(self, line: str) -> None:
+        self.function.code.append(line)
+
     def _mktemp(self, exp: str) -> str:
         temp = self.gensym()
-        # self.code.append(f"struct gc_obj* {temp} = {exp};")
-        self.code.append(f"GC_HANDLE(struct gc_obj*, {temp}, {exp});")
+        self._emit(f"GC_HANDLE(struct gc_obj*, {temp}, {exp});")
         return temp
 
     def compile_assign(self, env: Env, exp: Assign) -> Env:
@@ -60,8 +99,20 @@ class Compiler:
             items = [self.compile(env, item) for item in exp.items]
             result = self._mktemp(f"mklist(heap, {num_items})")
             for i, item in enumerate(items):
-                self.code.append(f"list_set({result}, {i}, {item});")
+                self._emit(f"list_set({result}, {i}, {item});")
             return result
+        if isinstance(exp, Function):
+            fields = free_in(exp)
+            name = self.gensym()
+            assert isinstance(exp.arg, Var)
+            closure = Closure(name, params=[exp.arg.name], fields=sorted(fields))
+            self.functions.append(closure)
+            cur = self.function
+            self.function = closure
+            val = self.compile({exp.arg.name: exp.arg.name}, exp.body)
+            closure.code.append(f"return {val};")
+            self.function = cur
+            return name
         raise NotImplementedError(f"exp {type(exp)} {exp}")
 
 
@@ -69,27 +120,36 @@ program = parse(
     tokenize(
         """
 println l
-. l = [a, b, c, a + b + c]
+. l = [inc a, b, c, a + b + c]
 . a = 1
 . b = 2
 . c = 3
 . print = runtime "print"
 . println = runtime "println"
+. inc = x -> x + 1
 """
     )
 )
 
 
 def main() -> None:
-    compiler = Compiler()
-    compiler.compile({}, program)
+    main = Closure("scrap_main", params=[])
+    compiler = Compiler(main)
+    result = compiler.compile({}, program)
+    main.code.append(f"return {result};")
     with open("output.c", "w") as f:
         print('#include "runtime.c"\n', file=f)
+        # Declare all functions
+        for function in compiler.functions:
+            print(function.decl() + ";", file=f)
+        for function in compiler.functions:
+            print(f"{function.decl()} {{", file=f)
+            for line in function.code:
+                print(line, file=f)
+            print("}", file=f)
         print("int main() {", file=f)
-        print("HANDLES();", file=f)
         print("heap = make_heap(1024);", file=f)
-        for line in compiler.code:
-            print(line, file=f)
+        print("scrap_main();", file=f)
         print("destroy_heap(heap);", file=f)
         print("}", file=f)
 

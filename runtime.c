04b4b734e7f554c5db382c825c921d913427cd32
Max Bernstein <max@bernsteinbear.com>
2024-05-17 15:02:11 -0400

    Add some asserts

--- runtime.c
+++ runtime.c
@@ -24,6 +24,8 @@ struct gc_obj* forwarded(struct gc_obj *obj) {
 }
 void forward(struct gc_obj *from, struct gc_obj *to) {
   fprintf(stderr, "forward: %p -> %p\n", from, to);
+  assert(!is_forwarded(from));
+  assert((((uintptr_t)to) & NOT_FORWARDED_BIT) == 0);
   from->tag = (uintptr_t)to;
 }
 

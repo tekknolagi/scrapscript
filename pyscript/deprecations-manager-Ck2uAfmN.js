0499676f97bd23095613719e02c1b04b5be31b9e
Max Bernstein <max@bernsteinbear.com>
2024-05-23 13:46:25 -0400

    Vendor PyScript

---
+++ pyscript/deprecations-manager-Ck2uAfmN.js
@@ -0,0 +1,2 @@
+import{e}from"./core-ovOoHZXs.js";import{notify as o}from"./error-DWfm2J-T.js";function r(){const e=document.querySelectorAll("script");for(const o of e)s(o.src)}function s(e){/\/pyscript\.net\/latest/.test(e)&&o("Loading scripts from latest is deprecated and will be removed soon. Please use a specific version instead.")}e.main.onReady.add(r),e.main.onWorker.add(r);
+//# sourceMappingURL=deprecations-manager-Ck2uAfmN.js.map
--- webrepl.py
+++ webrepl.py
@@ -15,90 +15,23 @@ logger = logging.getLogger(__name__)
 
 
 class ScrapReplServer(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self) -> None:
-        logger.debug("GET %s", self.path)
-        parsed_path = urllib.parse.urlsplit(self.path)
-        query = urllib.parse.parse_qs(parsed_path.query)
-        logger.debug("PATH %s", parsed_path)
-        logger.debug("QUERY %s", query)
-        if parsed_path.path == "/repl":
-            return self.do_repl()
-        if parsed_path.path == "/scrapscript.py":
-            return self.do_scrapscript_py()
-        if parsed_path.path == "/eval":
-            try:
-                return self.do_eval(query)
-            except Exception as e:
-                self.send_response(400)
-                self.send_header("Content-type", "text/plain")
-                self.end_headers()
-                self.wfile.write(str(e).encode("utf-8"))
-                return
-        if parsed_path.path == "/style.css":
-            self.send_response(200)
-            self.send_header("Content-type", "text/css")
-            self.end_headers()
-            with open(os.path.join(ASSET_DIR, "style.css"), "rb") as f:
-                self.wfile.write(f.read())
-            return
-        return self.do_404()
-
-    def do_repl(self) -> None:
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
+    def end_headers(self) -> None:
+        # Enable Cross-Origin Resource Sharing (CORS)
         self.send_header("Access-Control-Allow-Origin", "*")
         self.send_header("Cross-Origin-Opener-Policy", "same-origin")
         self.send_header("Cross-Origin-Embedder-Policy", "require-corp")
-        self.end_headers()
-        with open(os.path.join(ASSET_DIR, "repl.html"), "rb") as f:
-            self.wfile.write(f.read())
-        return
+        super().end_headers()
 
-    def do_scrapscript_py(self) -> None:
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-        with open(os.path.join(ASSET_DIR, "scrapscript.py"), "rb") as f:
-            self.wfile.write(f.read())
-        return
+    def do_GET(self) -> None:
+        parsed_path = urllib.parse.urlsplit(self.path)
+        if parsed_path.path == "/repl":
+            self.path = "/repl.html"
+        return super().do_GET()
 
-    def do_404(self) -> None:
-        self.send_response(404)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(b"""try hitting <a href="/repl">/repl</a>""")
-        return
-
-    def do_eval(self, query: Dict[str, Any]) -> None:
-        exp = query.get("exp")
-        if exp is None:
-            raise TypeError("Need expression to evaluate")
-        if len(exp) != 1:
-            raise TypeError("Need exactly one expression to evaluate")
-        exp = exp[0]
-        tokens = tokenize(exp)
-        ast = parse(tokens)
-        env = query.get("env")
-        if env is None:
-            env = STDLIB
-        else:
-            if len(env) != 1:
-                raise TypeError("Need exactly one env")
-            env_object = deserialize(env[0])
-            assert isinstance(env_object, EnvObject)
-            env = env_object.env
-        logger.debug("env is %s", env)
-        monad = ScrapMonad(env)
-        result, next_monad = monad.bind(ast)
-        serialized = EnvObject(next_monad.env).serialize()
-        encoded = bencode(serialized)
-        response = {"env": encoded.decode("utf-8"), "result": str(result)}
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.send_header("Cache-Control", "max-age=3600")
-        self.end_headers()
-        self.wfile.write(json.dumps(response).encode("utf-8"))
-        return
+    def send_error(self, code: int, message: str | None = None, explain: str | None = None) -> None:
+        if code == 404:
+            self.error_message_format = """try hitting <a href="/repl">/repl</a>"""
+        return super().send_error(code, message)
 
 
 def serve(args: argparse.Namespace) -> None:

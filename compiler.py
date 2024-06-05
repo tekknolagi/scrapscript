0fec23e60791cea623d343fd1ccd840ef08f690d
Max Bernstein <max@bernsteinbear.com>
2024-06-05 13:06:13 -0400

    .

--- compiler.py
+++ compiler.py
@@ -436,7 +436,6 @@ def compile_to_binary(source: str, memory: int, debug: bool) -> str:
     with tempfile.NamedTemporaryFile(mode="w", suffix=".c", delete=False) as c_file:
         outdir = os.path.dirname(c_file.name)
         shutil.copy("runtime.c", outdir)
-        shutil.copy("cli.c", outdir)
         c_file.write(c_code)
         with open("cli.c", "r") as f:
             c_file.write(f.read())

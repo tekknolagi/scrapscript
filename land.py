1ccf8637d0216d0661a6e312cd0becfb4ac80665
Max Bernstein <max@bernsteinbear.com>
2024-07-10 14:57:58 -0400

    Use fully-qualified bottle names

--- land.py
+++ land.py
@@ -1,8 +1,10 @@
+import bottle
 import hashlib
 import sqlite3
-from bottle import get, post, run, template, request, response
+
 
 IN_MEMORY = False
+
 
 if IN_MEMORY:
     db = sqlite3.connect(":memory:")
@@ -15,7 +17,7 @@ with db:
     cursor.execute("CREATE UNIQUE INDEX IF NOT EXISTS scrap_contents on scrap_object ( contents )")
 
 
-@get("/scrap/sha256/<hash>")
+@bottle.get("/scrap/sha256/<hash>")
 def find_scrap(hash):
     with db:
         cursor = db.cursor()
@@ -23,23 +25,23 @@ def find_scrap(hash):
         row = cursor.fetchone()
         if row:
             flat = row[0]
-            response.status = 200
-            response.content_type = "application/scrapscript"
-            response.content_length = len(flat)
+            bottle.response.status = 200
+            bottle.response.content_type = "application/scrapscript"
+            bottle.response.content_length = len(flat)
             return flat
         else:
-            response.status = 404
+            bottle.response.status = 404
             return "Not found.\n"
 
 
-@post("/scrap/upload")
+@bottle.post("/scrap/upload")
 def upload_scrap():
     # curl -X POST -F smallstr=@smallstr.flat -F foo=bar -F a=b  ...
     # for key, value in request.forms.items():
     #     print(key, value)
     inserted = set()
     items = []
-    for key, value in request.files.items():
+    for key, value in bottle.request.files.items():
         flat = value.file.read()
         hash = hashlib.sha256(flat).hexdigest()
         items.append({"hash": hash, "contents": flat})
@@ -48,14 +50,15 @@ def upload_scrap():
         result = cursor.executemany("INSERT or IGNORE INTO scrap_object VALUES (:hash, :contents)", items)
         num_inserted = result.rowcount
     if num_inserted > 0:
-        response.status = 201
+        bottle.response.status = 201
         return {"num_inserted": num_inserted}
-    response.status = 304
+    bottle.response.status = 304
     return {"num_inserted": 0}
 
 
 if IN_MEMORY:
-    @post("/persist")
+
+    @bottle.post("/persist")
     def persist_db():
         disk_db = sqlite3.connect("land.db")
         db.backup(disk_db)
@@ -63,4 +66,4 @@ if IN_MEMORY:
 
 
 if __name__ == "__main__":
-    run(host="localhost", port=8080, reloader=True)
+    bottle.run(host="localhost", port=8080, reloader=True)

bea0d01338ccfb8c0b8b1eec696e4d13acf5d650
Max Bernstein <max@bernsteinbear.com>
2024-09-24 14:12:54 -0400

    mypy

--- lc.py
+++ lc.py
@@ -26,7 +26,7 @@ class MonoType(Ty):
 class TyVar(MonoType):
     name: str
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"'{self.name}"
 
 
@@ -35,7 +35,7 @@ class TyCon(MonoType):
     name: str
     args: list[MonoType]
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         if not self.args:
             return self.name
         return f"({self.name.join(map(repr, self.args))})"
@@ -46,7 +46,7 @@ class Forall(Ty):
     tyvars: list[TyVar]
     ty: Ty
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"(forall {', '.join(map(repr, self.tyvars))}. {self.ty})"
 
 
@@ -58,25 +58,49 @@ NotFunc = TyCon("->", [BoolType, BoolType])
 
 
 class ReprTest(unittest.TestCase):
-    def test_tyvar(self):
+    def test_tyvar(self) -> None:
         self.assertEqual(repr(TyVar("a")), "'a")
 
-    def test_tycon(self):
+    def test_tycon(self) -> None:
         self.assertEqual(repr(TyCon("int", [])), "int")
 
-    def test_tycon_args(self):
+    def test_tycon_args(self) -> None:
         self.assertEqual(repr(TyCon("->", [IntType, IntType])), "(int->int)")
 
-    def test_forall(self):
+    def test_forall(self) -> None:
         self.assertEqual(repr(Forall([TyVar("a"), TyVar("b")], TyVar("a"))), "(forall 'a, 'b. 'a)")
 
 
-def func_type(*args):
+def func_type(*args: MonoType) -> TyCon:
     return TyCon("->", list(args))
 
 
-def tuple_type(*args):
+def tuple_type(*args: MonoType) -> TyCon:
     return TyCon("*", list(args))
+
+
+def ftv_ty(ty: Ty) -> set[str]:
+    if isinstance(ty, TyVar):
+        return {ty.name}
+    if isinstance(ty, TyCon):
+        return set().union(*map(ftv_ty, ty.args))
+    if isinstance(ty, Forall):
+        return ftv_ty(ty.ty) - set(tyvar.name for tyvar in ty.tyvars)
+    raise TypeError(f"Unknown type: {ty}")
+
+
+class FtvTest(unittest.TestCase):
+    def test_tyvar(self) -> None:
+        self.assertEqual(ftv_ty(TyVar("a")), {"a"})
+
+    def test_tycon(self) -> None:
+        self.assertEqual(ftv_ty(TyCon("int", [])), set())
+
+    def test_tycon_args(self) -> None:
+        self.assertEqual(ftv_ty(TyCon("->", [TyVar("a"), TyVar("b")])), {"a", "b"})
+
+    def test_forall(self) -> None:
+        self.assertEqual(ftv_ty(Forall([TyVar("a"), TyVar("b")], TyVar("a"))), set())
 
 
 if __name__ == "__main__":

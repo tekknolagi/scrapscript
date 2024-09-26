c24261c0b99c37b7857d49e053994bce8692d31b
Max Bernstein <max@bernsteinbear.com>
2024-09-26 11:32:31 -0400

    Add Algorithm J

--- lc.py
+++ lc.py
@@ -17,7 +17,20 @@ from scrapscript import (
 
 @dataclasses.dataclass
 class Ty:
-    pass
+    forwarded: Ty | None = dataclasses.field(init=False, default=None)
+
+    def find(self) -> Ty:
+        result: Ty = self
+        while isinstance(result, TyVar):
+            it = result.forwarded
+            if it is None:
+                return result
+            result = it
+        return result
+
+    def _set_forwarded(self, other: Ty) -> None:
+        assert type(self) == type(other)
+        assert self == other, f"{self} != {other}"
 
 
 @dataclasses.dataclass
@@ -31,6 +44,12 @@ class TyVar(MonoType):
 
     def __repr__(self) -> str:
         return f"'{self.name}"
+
+    def make_equal_to(self, other: Ty) -> None:
+        self.find()._set_forwarded(other)
+
+    def _set_forwarded(self, other: Ty) -> None:
+        self.forwarded = other
 
 
 @dataclasses.dataclass
@@ -204,6 +223,25 @@ class FreshTests(unittest.TestCase):
         self.assertEqual(fresh_tyvar(), TyVar("t0"))
         self.assertEqual(fresh_tyvar("x"), TyVar("x1"))
 
+    def assertTyEqual(self, l: Ty, r: Ty) -> bool:
+        l = l.find()
+        r = r.find()
+        if isinstance(l, TyVar) and isinstance(r, TyVar):
+            if l != r:
+                self.fail(f"Type mismatch: {l} != {r}")
+            return True
+        if isinstance(l, TyCon) and isinstance(r, TyCon):
+            if l.name != r.name:
+                self.fail(f"Type mismatch: {l} != {r}")
+            if len(l.args) != len(r.args):
+                self.fail(f"Type mismatch: {l} != {r}")
+            for l_arg, r_arg in zip(l.args, r.args):
+                self.assertTyEqual(l_arg, r_arg)
+            return True
+        if isinstance(l, Forall) or isinstance(r, Forall):
+            raise NotImplementedError
+        self.fail(f"Type mismatch: {l} != {r}")
+
 
 def bind_var(ty: MonoType, name: str) -> Subst:
     if isinstance(ty, TyVar) and ty.name == name:
@@ -270,11 +308,11 @@ def unify_fail(ty1: MonoType, ty2: MonoType) -> None:
     raise TypeError(f"Unification failed for {ty1} and {ty2}")
 
 
-def unify(ty1: MonoType, ty2: MonoType) -> Subst:
+def unify_w(ty1: MonoType, ty2: MonoType) -> Subst:
     if isinstance(ty1, TyVar):
         return bind_var(ty2, ty1.name)
     if isinstance(ty2, TyVar):  # Mirror
-        return unify(ty2, ty1)
+        return unify_w(ty2, ty1)
     if isinstance(ty1, TyCon) and isinstance(ty2, TyCon):
         if ty1.name != ty2.name:
             unify_fail(ty1, ty2)
@@ -283,41 +321,41 @@ def unify(ty1: MonoType, ty2: MonoType) -> Subst:
         result: Subst = {}
         for l, r in zip(ty1.args, ty2.args):
             result = compose(
-                unify(apply_ty(l, result), apply_ty(r, result)),
+                unify_w(apply_ty(l, result), apply_ty(r, result)),
                 result,
             )
         return result
     raise TypeError(f"ICE: Unexpected type {type(ty1)}")
 
 
-class UnifyTests(FreshTests):
+class UnifyWTests(FreshTests):
     def test_tyvar_tyvar(self) -> None:
-        self.assertEqual(unify(TyVar("a"), TyVar("b")), {"a": TyVar("b")})
-        self.assertEqual(unify(TyVar("b"), TyVar("a")), {"b": TyVar("a")})
+        self.assertEqual(unify_w(TyVar("a"), TyVar("b")), {"a": TyVar("b")})
+        self.assertEqual(unify_w(TyVar("b"), TyVar("a")), {"b": TyVar("a")})
 
     def test_tyvar_tycon(self) -> None:
-        self.assertEqual(unify(TyVar("a"), IntType), {"a": IntType})
-        self.assertEqual(unify(IntType, TyVar("a")), {"a": IntType})
+        self.assertEqual(unify_w(TyVar("a"), IntType), {"a": IntType})
+        self.assertEqual(unify_w(IntType, TyVar("a")), {"a": IntType})
 
     def test_tycon_tycon_name_mismatch(self) -> None:
         with self.assertRaisesRegex(TypeError, "Unification failed"):
-            unify(IntType, BoolType)
+            unify_w(IntType, BoolType)
 
     def test_tycon_tycon_arity_mismatch(self) -> None:
         l = TyCon("x", [TyVar("a")])
         r = TyCon("x", [])
         with self.assertRaisesRegex(TypeError, "Unification failed"):
-            unify(l, r)
+            unify_w(l, r)
 
     def test_tycon_tycon_unifies_arg(self) -> None:
         l = TyCon("x", [TyVar("a")])
         r = TyCon("x", [TyVar("b")])
-        self.assertEqual(unify(l, r), {"a": TyVar("b")})
+        self.assertEqual(unify_w(l, r), {"a": TyVar("b")})
 
     def test_tycon_tycon_unifies_args(self) -> None:
         l = func_type(TyVar("a"), TyVar("b"))
         r = func_type(TyVar("c"), TyVar("d"))
-        self.assertEqual(unify(l, r), {"a": TyVar("c"), "b": TyVar("d")})
+        self.assertEqual(unify_w(l, r), {"a": TyVar("c"), "b": TyVar("d")})
 
 
 def infer_w(expr: Object, ctx: Context) -> tuple[Subst, MonoType]:
@@ -338,7 +376,7 @@ def infer_w(expr: Object, ctx: Context) -> tuple[Subst, MonoType]:
         s1, ty = infer_w(expr.func, ctx)
         s2, p = infer_w(expr.arg, apply_ctx(ctx, s1))
         r = fresh_tyvar("a")
-        s3 = unify(apply_ty(ty, s2), TyCon("->", [p, r]))
+        s3 = unify_w(apply_ty(ty, s2), TyCon("->", [p, r]))
         return compose(compose(s3, s2), s1), apply_ty(r, s3)
     if isinstance(expr, Binop):
         left, right = expr.left, expr.right
@@ -408,6 +446,100 @@ class InferWTests(FreshTests):
         )
 
 
+def unify_j(ty1: Ty, ty2: Ty) -> None:
+    ty1 = ty1.find()
+    ty2 = ty2.find()
+    if isinstance(ty1, TyVar):
+        ty1.make_equal_to(ty2)
+        return
+    if isinstance(ty2, TyVar):  # Mirror
+        return unify_j(ty2, ty1)
+    if isinstance(ty1, TyCon) and isinstance(ty2, TyCon):
+        if ty1.name != ty2.name:
+            unify_fail(ty1, ty2)
+            return
+        if len(ty1.args) != len(ty2.args):
+            unify_fail(ty1, ty2)
+            return
+        for l, r in zip(ty1.args, ty2.args):
+            unify_j(l, r)
+        return
+    raise TypeError(f"ICE: Unexpected type {type(ty1)}")
+
+
+class UnifyJTests(FreshTests):
+    def test_tyvar_tyvar(self) -> None:
+        a = TyVar("a")
+        b = TyVar("b")
+        unify_j(a, b)
+        self.assertIs(a.find(), b.find())
+
+    def test_tyvar_tycon(self) -> None:
+        a = TyVar("a")
+        unify_j(a, IntType)
+        self.assertIs(a.find(), IntType)
+        b = TyVar("b")
+        unify_j(b, IntType)
+        self.assertIs(b.find(), IntType)
+
+    def test_tycon_tycon_name_mismatch(self) -> None:
+        with self.assertRaisesRegex(TypeError, "Unification failed"):
+            unify_j(IntType, BoolType)
+
+    def test_tycon_tycon_arity_mismatch(self) -> None:
+        l = TyCon("x", [TyVar("a")])
+        r = TyCon("x", [])
+        with self.assertRaisesRegex(TypeError, "Unification failed"):
+            unify_j(l, r)
+
+    def test_tycon_tycon_unifies_arg(self) -> None:
+        a = TyVar("a")
+        b = TyVar("b")
+        l = TyCon("x", [a])
+        r = TyCon("x", [b])
+        unify_j(l, r)
+        self.assertIs(a.find(), b.find())
+
+    def test_tycon_tycon_unifies_args(self) -> None:
+        a, b, c, d = map(TyVar, "abcd")
+        l = func_type(a, b)
+        r = func_type(c, d)
+        unify_j(l, r)
+        self.assertIs(a.find(), c.find())
+        self.assertIs(b.find(), d.find())
+        self.assertIsNot(a.find(), b.find())
+
+
+def infer_j(expr: Object, ctx: Context) -> TyVar:
+    result = fresh_tyvar()
+    if isinstance(expr, Var):
+        scheme = ctx.get(expr.name)
+        if scheme is None:
+            raise TypeError(f"Unbound variable {expr.name}")
+        unify_j(result, instantiate(scheme))
+        return result
+    if isinstance(expr, Int):
+        unify_j(result, IntType)
+        return result
+    if isinstance(expr, Function):
+        arg_tyvar = fresh_tyvar("a")
+        assert isinstance(expr.arg, Var)
+        body_ctx = {**ctx, expr.arg.name: Forall([], arg_tyvar)}
+        body_ty = infer_j(expr.body, body_ctx)
+        unify_j(result, func_type(arg_tyvar, body_ty))
+        return result
+    if isinstance(expr, Apply):
+        func_ty = infer_j(expr.func, ctx)
+        arg_ty = infer_j(expr.arg, ctx)
+        unify_j(func_ty, TyCon("->", [arg_ty, result]))
+        return result
+    if isinstance(expr, Binop):
+        left, right = expr.left, expr.right
+        op = Var(BinopKind.to_str(expr.op))
+        return infer_j(Apply(Apply(op, left), right), ctx)
+    raise TypeError(f"Unexpected type {type(expr)}")
+
+
 class BaseTestCases:
     class InferTestsBase(FreshTests):
         def infer(self, expr: Object, ctx: Context) -> Ty:
@@ -431,11 +563,11 @@ class BaseTestCases:
 
         def test_nested_function_outer(self) -> None:
             ty = self.infer(Function(Var("x"), Function(Var("y"), Var("x"))), {})
-            self.assertEqual(ty, func_type(TyVar("a0"), func_type(TyVar("a1"), TyVar("a0"))))
+            self.assertEqual(ty, func_type(TyVar("a0"), TyVar("a1"), TyVar("a0")))
 
         def test_nested_function_inner(self) -> None:
             ty = self.infer(Function(Var("x"), Function(Var("y"), Var("y"))), {})
-            self.assertEqual(ty, func_type(TyVar("a0"), func_type(TyVar("a1"), TyVar("a1"))))
+            self.assertEqual(ty, func_type(TyVar("a0"), TyVar("a1"), TyVar("a1")))
 
         def test_apply_id_int(self) -> None:
             func = Function(Var("x"), Var("x"))
@@ -471,6 +603,61 @@ class InferWSideBySideTests(BaseTestCases.InferTestsBase):
     def infer(self, expr: Object, ctx: Context) -> Ty:
         _, ty = infer_w(expr, ctx)
         return ty
+
+
+class InferJSideBySideTests(FreshTests):
+    def test_unbound_var(self) -> None:
+        with self.assertRaisesRegex(TypeError, "Unbound variable"):
+            infer_j(Var("a"), {})
+
+    def test_var_instantiates_scheme(self) -> None:
+        ty = infer_j(Var("a"), {"a": Forall([TyVar("b")], TyVar("b"))}).find()
+        self.assertEqual(ty, TyVar("t1"))
+
+    def test_int(self) -> None:
+        ty = infer_j(Int(123), {}).find()
+        self.assertEqual(ty, IntType)
+
+    def test_function_returns_arg(self) -> None:
+        ty = infer_j(Function(Var("x"), Var("x")), {}).find()
+        self.assertTyEqual(ty, func_type(TyVar("a1"), TyVar("a1")))
+
+    def test_nested_function_outer(self) -> None:
+        ty = infer_j(Function(Var("x"), Function(Var("y"), Var("x"))), {}).find()
+        self.assertTyEqual(ty, func_type(TyVar("a1"), TyVar("a3"), TyVar("a1")))
+
+    def test_nested_function_inner(self) -> None:
+        ty = infer_j(Function(Var("x"), Function(Var("y"), Var("y"))), {}).find()
+        self.assertTyEqual(ty, func_type(TyVar("a1"), TyVar("a3"), TyVar("a3")))
+
+    def test_apply_id_int(self) -> None:
+        func = Function(Var("x"), Var("x"))
+        arg = Int(123)
+        ty = infer_j(Apply(func, arg), {}).find()
+        self.assertIs(ty, IntType)
+
+    def test_apply_two_arg_returns_function(self) -> None:
+        func = Function(Var("x"), Function(Var("y"), Var("x")))
+        arg = Int(123)
+        ty = infer_j(Apply(func, arg), {}).find()
+        self.assertTyEqual(ty, func_type(TyVar("a4"), IntType))
+
+    def test_binop_add_constrains_int(self) -> None:
+        expr = Binop(BinopKind.ADD, Var("x"), Var("y"))
+        ty = infer_j(
+            expr,
+            {
+                "x": Forall([], TyVar("a")),
+                "y": Forall([], TyVar("b")),
+                "+": Forall([], func_type(IntType, IntType, IntType)),
+            },
+        )
+        self.assertEqual(ty.find(), IntType)
+
+    def test_binop_add_function_constrains_int(self) -> None:
+        expr = Function(Var("x"), Function(Var("y"), Binop(BinopKind.ADD, Var("x"), Var("y"))))
+        ty = infer_j(expr, {"+": Forall([], func_type(IntType, IntType, IntType))})
+        self.assertTyEqual(ty, func_type(IntType, IntType, IntType))
 
 
 if __name__ == "__main__":

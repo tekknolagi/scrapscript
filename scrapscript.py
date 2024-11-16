b93b320a4edb762cc05c7aa7d5b8749faa6d4d70
Max Bernstein <max@bernsteinbear.com>
2024-11-16 12:52:34 -0500

    Use eager level-based generalization

--- scrapscript.py
+++ scrapscript.py
@@ -3984,6 +3984,7 @@ class MonoType:
 @dataclasses.dataclass
 class TyVar(MonoType):
     forwarded: MonoType | None = dataclasses.field(init=False, default=None)
+    level: int = dataclasses.field(init=False, default_factory=lambda: current_level)
     name: str
 
     def find(self) -> MonoType:
@@ -4122,24 +4123,38 @@ def unify_fail(ty1: MonoType, ty2: MonoType) -> None:
     raise InferenceError(f"Unification failed for {ty1} and {ty2}")
 
 
-def occurs_in(tyvar: TyVar, ty: MonoType) -> bool:
+def occurs(tyvar: TyVar, ty: MonoType) -> None:
     if isinstance(ty, TyVar):
-        return tyvar == ty
+        if tyvar == ty:
+            raise InferenceError(f"Occurs check failed for {tyvar} and {ty}")
+        if ty.is_unbound():
+            min_level = min(tyvar.level, ty.level) if tyvar.is_unbound() else tyvar.level
+            ty.level = min_level
+            return
+        occurs(tyvar, ty.forwarded)
+        return
     if isinstance(ty, TyCon):
-        return any(occurs_in(tyvar, arg) for arg in ty.args)
+        for arg in ty.args:
+            occurs(tyvar, arg)
+        return
     if isinstance(ty, TyEmptyRow):
-        return False
+        return
     if isinstance(ty, TyRow):
-        return any(occurs_in(tyvar, val) for val in ty.fields.values()) or occurs_in(tyvar, ty.rest)
+        for val in ty.fields.values():
+            occurs(tyvar, val)
+        occurs(tyvar, ty.rest)
+        return
     raise InferenceError(f"Unknown type: {ty}")
 
 
 def unify_type(ty1: MonoType, ty2: MonoType) -> None:
+    if ty1 == ty2:
+        return
     ty1 = ty1.find()
     ty2 = ty2.find()
     if isinstance(ty1, TyVar):
-        if occurs_in(ty1, ty2):
-            raise InferenceError(f"Occurs check failed for {ty1} and {ty2}")
+        # ty1 is unbound if it's still a TyVar after .find()
+        occurs(ty1, ty2)
         ty1.make_equal_to(ty2)
         return
     if isinstance(ty2, TyVar):  # Mirror
@@ -4210,6 +4225,18 @@ Context = typing.Mapping[str, Forall]
 
 fresh_var_counter = 0
 
+current_level = 0
+
+
+def enter_level() -> None:
+    global current_level
+    current_level += 1
+
+
+def leave_level() -> None:
+    global current_level
+    current_level -= 1
+
 
 def fresh_tyvar(prefix: str = "t") -> TyVar:
     global fresh_var_counter
@@ -4248,28 +4275,26 @@ def instantiate(scheme: Forall) -> MonoType:
     return apply_ty(scheme.ty, fresh)
 
 
-def ftv_ty(ty: MonoType) -> set[str]:
-    ty = ty.find()
+def ftv_ty(ty: MonoType, min_level=-1) -> set[str]:
     if isinstance(ty, TyVar):
-        return {ty.name}
+        if ty.is_unbound():
+            if ty.level > min_level:
+                return {ty.name}
+        else:
+            return ftv_ty(ty.forwarded, min_level)
+        return set()
     if isinstance(ty, TyCon):
-        return set().union(*map(ftv_ty, ty.args))
+        return set().union(*(ftv_ty(arg, min_level) for arg in ty.args))
     if isinstance(ty, TyEmptyRow):
         return set()
     if isinstance(ty, TyRow):
-        return set().union(*map(ftv_ty, ty.fields.values()), ftv_ty(ty.rest))
-    raise InferenceError(f"Unknown type: {ty}")
+        return set().union(ftv_ty(ty.rest, min_level), *(ftv_ty(val, min_level) for val in ty.fields.values()))
+    raise InferenceError(f"ftv_ty: Unknown type: {ty}")
 
 
-def generalize(ty: MonoType, ctx: Context) -> Forall:
-    def ftv_scheme(ty: Forall) -> set[str]:
-        return ftv_ty(ty.ty) - set(tyvar.name for tyvar in ty.tyvars)
-
-    def ftv_ctx(ctx: Context) -> set[str]:
-        return set().union(*(ftv_scheme(scheme) for scheme in ctx.values()))
-
+def generalize(ty: MonoType) -> Forall:
     # TODO(max): Freshen?
-    tyvars = ftv_ty(ty) - ftv_ctx(ctx)
+    tyvars = ftv_ty(ty, current_level)
     return Forall([TyVar(name) for name in sorted(tyvars)], ty)
 
 
@@ -4355,6 +4380,7 @@ def infer_type(expr: Object, ctx: Context) -> MonoType:
     if isinstance(expr, Where):
         assert isinstance(expr.binding, Assign)
         name, value, body = expr.binding.name.name, expr.binding.value, expr.body
+        enter_level()
         if isinstance(value, (Function, MatchFunction)):
             # Letrec
             func_ty: MonoType = fresh_tyvar()
@@ -4362,7 +4388,8 @@ def infer_type(expr: Object, ctx: Context) -> MonoType:
         else:
             # Let
             value_ty = infer_type(value, ctx)
-        value_scheme = generalize(value_ty, ctx)
+        leave_level()
+        value_scheme = generalize(value_ty)
         body_ty = infer_type(body, {**ctx, name: value_scheme})
         return set_type(expr, body_ty)
     if isinstance(expr, List):

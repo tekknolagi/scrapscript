45d7fe8d74ddac7f1e060ac0283273f8feab6dd9
neuroevolutus <19356702+neuroevolutus@users.noreply.github.com>
2025-02-24 01:09:42 -0600

    Add source extent to int literal in test

--- scrapscript_tests.py
+++ scrapscript_tests.py
@@ -1268,6 +1268,9 @@ class ParserTests(unittest.TestCase):
             start=SourceLocation(lineno=1, colno=1, byteno=0), end=SourceLocation(lineno=1, colno=1, byteno=0)
         )
         num = IntLit(1)
+        num.source_extent = SourceExtent(
+            start=SourceLocation(lineno=1, colno=2, byteno=1), end=SourceLocation(lineno=1, colno=2, byteno=1)
+        )
         right_paren = RightParen()
         right_paren.source_extent = SourceExtent(
             start=SourceLocation(lineno=1, colno=3, byteno=2), end=SourceLocation(lineno=1, colno=3, byteno=2)
